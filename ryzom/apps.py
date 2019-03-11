31259fadfb8b882097c6d0fb20d7e128edf116af
tmig <tmig@yourlabs.org>
2019-03-11 17:49:19 +0100

    Fixing #3

--- ryzom/apps.py
+++ ryzom/apps.py
@@ -1,13 +1,18 @@
 from django.apps import AppConfig
+from django.db import OperationalError, ProgrammingError
 
 
 class RyzomConfig(AppConfig):
     name = 'ryzom'
 
     def ready(self):
-        import ryzom.signals
+        import ryzom.signals # noqa
         from ryzom.pubsub import to_publish
-        Clients = self.get_model('Clients')
-        Clients.objects.all().delete()
-        for publication in to_publish:
-            publication.publish_all()
+
+        try:
+            Clients = self.get_model('Clients')
+            Clients.objects.all().delete()
+            for publication in to_publish:
+                publication.publish_all()
+        except (OperationalError, ProgrammingError):
+            pass
