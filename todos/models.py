65c7b21bc4397f1313de94aa948cab60e75623e9
John Kirkwood <jkirkwood@kclinfo.com>
2019-08-07 19:19:58 +0100

    Prefer AUTH_USER_MODEL in Task.

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 from ryzom.pubsub import Publishable
 
 
 class Task(models.Model, Publishable):
-    user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(
+        getattr(settings, 'AUTH_USER_MODEL', 'auth.User'),
+        on_delete=models.SET_NULL, blank=True, null=True)
     about = models.CharField(max_length=1024)
 
 
