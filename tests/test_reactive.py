96db957f03c6386eac5790c7d59efb118abd040d
tmig <tmig@yourlabs.org>
2021-03-12 19:36:01 +0100

    rien

---
+++ tests/test_reactive.py
@@ -0,0 +1,10 @@
+import pytest
+
+from django.conf import settings
+from django import views
+
+from ryzom import html
+from ryzom_django.views import ReactiveMixin
+
+
+reactive = pytest.mark.skipif(not settings.CHANNELS_ENABLE, reason='Reactive disabled')
