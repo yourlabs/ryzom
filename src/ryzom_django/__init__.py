143ba5373c9775e34820d2310c2006b1504bcfcb
tmig <tmig@yourlabs.org>
2021-03-12 16:24:03 +0100

    fix apps

--- src/ryzom_django/__init__.py
+++ src/ryzom_django/__init__.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-if settings.CHANNELS_ENABLE:
+if 'channels' in settings.INSTALLED_APPS:
     default_app_config = 'ryzom_django.apps.ReactiveConfig'
 else:
     default_app_config = 'ryzom_django.apps.BaseConfig'
