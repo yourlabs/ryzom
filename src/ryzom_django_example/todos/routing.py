092070e8dad2ecaa6ca0904d0fdd66e2b8b17eee
tmig <tmig@yourlabs.org>
2021-03-13 10:10:17 +0100

    menage

--- src/ryzom_django_example/todos/routing.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.urls import re_path
-
-from .views import Layout
-
-urlpatterns = [
-    re_path(settings.RYZOM_APP, Layout),
-    re_path(r'', Layout),
-]
