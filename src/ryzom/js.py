24eba05ecca324b58a4dd5b41e8224a3366cf8fe
jpic <jpic@yourlabs.org>
2021-03-15 11:22:02 +0100

    Refactor bundle

--- src/ryzom/js.py
+++ src/ryzom/js.py
@@ -2,22 +2,22 @@ import importlib
 import py2js
 
 
+def webcomponent(value):
+    return [
+        py2js.transpile_class(
+            value.HTMLElement,
+            superclass='HTMLElement',
+            newname=value.__name__,
+        ),
+        f'window.customElements.define("{value.tag}", {value.__name__});',
+    ]
+
+
 def bundle(*modules):
     out = []
     for module in modules:
         mod = importlib.import_module(module)
         for key, value in mod.__dict__.items():
-            if not hasattr(value, 'HTMLElement'):
-                continue
-            if not hasattr(value, 'tag'):
-                continue
-
-            out.append(
-                py2js.transpile_class(
-                    value.HTMLElement,
-                    superclass='HTMLElement',
-                    newname=value.__name__,
-                )
-            )
-            out.append(f'window.customElements.define("{value.tag}", {value.__name__});')
+            if hasattr(value, 'HTMLElement') and hasattr(value, 'tag'):
+                out += webcomponent(value)
     return '\n'.join(out)
