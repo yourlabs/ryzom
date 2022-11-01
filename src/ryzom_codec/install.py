dd5f45bb0c51ab39f98db3efe5f5763357776e64
Maxime Lalisse <max@technostructures.org>
2022-11-01 20:39:10 +0100

    feat: try catch for pyx encoding

--- src/ryzom_codec/install.py
+++ src/ryzom_codec/install.py
@@ -9,23 +9,33 @@ import site
 
 PTH_FILENAME = "ryzom.pth"
 PTH_CONTENT = (
-    "import ryzom_codec.register"
-#    "import sys; exec('"
-#    "try:\\n"
-#    "    import ryzom.codec.register\\n"
-#    "except ImportError:\\n"
-#    "    pass\\n"
-#    "')"
+#    "import ryzom_codec.register"
+    "import sys; exec('"
+    "try:\\n"
+    "    import ryzom_codec.register\\n"
+    "except ImportError:\\n"
+    "    pass\\n"
+    "')"
 )
 
-python_lib = get_python_lib()
-with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
-    pth_file.write(PTH_CONTENT)
+try:
+    python_lib = get_python_lib()
+    with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
+        pth_file.write(PTH_CONTENT)
+except Exception:
+    pass
 
-python_lib = get_python_lib(prefix=os.getenv("HOME") + "/.local")
-with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
-    pth_file.write(PTH_CONTENT)
+try:
+    python_lib = get_python_lib(prefix=os.getenv("HOME") + "/.local")
+    with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
+        pth_file.write(PTH_CONTENT)
+except Exception:
+    pass
 
-python_lib = site.getsitepackages()[0]
-with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
-    pth_file.write(PTH_CONTENT)
+try:
+    python_lib = site.getsitepackages()[0]
+    with open(os.path.join(python_lib, PTH_FILENAME), "w") as pth_file:
+        pth_file.write(PTH_CONTENT)
+except Exception:
+    pass
+
